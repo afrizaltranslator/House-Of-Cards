0d044ca7cd5d61cc84720022bb086c24b14b2395
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2024-02-10 11:14:18 +0100

    Crap begone!

--- Run.ps1
+++
@@ -1,1 +0,0 @@
-E:/Projects/Applications/Slyvina/Apps/SCI/SCI_Exe/SCI_Run.exe "Debug/House of Cards.jql"
--- b.ps1
+++
@@ -1,1 +0,0 @@
-E:\Projects\Applications\Slyvina\Apps\SCI\SCI_Exe\SCI_Build src/Scripts/Project/HouseOfCards.ini -debug -scyndidebug -scyndiforce
